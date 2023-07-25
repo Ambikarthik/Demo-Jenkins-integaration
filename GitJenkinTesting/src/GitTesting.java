746af1b16d1fb0aa75d2139f357dcc4d61b22990
Ambikarthik <110320285+Ambikarthik@users.noreply.github.com>
2023-07-25 16:24:39 +0530

    Add files via upload

--- GitJenkinTesting/src/GitTesting.java
+++ GitJenkinTesting/src/GitTesting.java
@@ -3,7 +3,7 @@ public class GitTesting {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Welcome to GIT");
+		System.out.println("Welcome to GITHUB");
 	}
 
 }
